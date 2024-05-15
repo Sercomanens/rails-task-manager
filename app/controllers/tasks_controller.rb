fb0403f9620eeecc4c5ef06e012ed641b6930949
Laurent Sercomanens <laurent.sercomanens@gmail.com>
2024-05-15 21:33:26 +0530

    delete files and modify files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,53 @@
 class TasksController < ApplicationController
-  def list
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def index
     @tasks = Task.all
   end
 
-  def find
+  def show
+    @task = Task.find_by(id: params[:id])
+
+    if @task.nil?
+      flash[:alert] = "Task not found"
+      redirect_to tasks_path
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
     @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
-  private
-
-  def task_params
-    params.require(:task).permit(:title, :details, :completed)
+  def new
+    @task = Task.new
   end
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+end
+
+
+
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
 end
